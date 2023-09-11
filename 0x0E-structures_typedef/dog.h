65787fef83bc9dd4efc220f06e36e6d992f664a1
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-09-11 16:49:03 +0200

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -26,3 +27,5 @@ int _strlen(char *s);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 char *_strcpy(char *dest, char *src);
+
+#endif
