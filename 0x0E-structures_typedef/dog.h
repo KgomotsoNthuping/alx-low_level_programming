64117113fad5b40867b32e317bdff703edc4205a
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-09-11 16:41:58 +0200

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: element
+ * @age: element
+ * @owner: element
+ *
+ * Description: woof
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
