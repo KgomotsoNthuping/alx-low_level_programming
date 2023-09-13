ac7b832df50ae4f9237aa7856a723ef53c2fc07c
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-09-13 23:25:07 +0200

    next

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,4 @@
+#ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
 #include <stddef.h>
@@ -7,3 +8,5 @@ int _putchar(char n);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
