6d0583910c03b856933dee620beba373650e0cac
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-09-21 21:00:45 +0200

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,12 +2,11 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the nodes
- * @h: pointer to list_t
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed
  */
-
 size_t print_list(const list_t *h)
 {
  size_t s = 0;
