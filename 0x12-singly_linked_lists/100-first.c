820b7750d554a1d6f30ff8f5df5b91a43a944757
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-09-21 21:18:21 +0200

    la

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints words before the main
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
