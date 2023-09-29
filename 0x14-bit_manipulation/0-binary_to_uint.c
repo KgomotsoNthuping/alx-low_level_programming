5270647c3afc2f340a674decc94fb157a4a51a1e
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-09-29 04:18:33 +0200

    struggle

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary num
- * @b: string 
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to string
  *
- * Return: binary number
+ * Return: the converted number, or 0
  */
 
 unsigned int binary_to_uint(const char *b)
