3429bf35431d6233a92c44682a2710a644ac6747
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-11-14 09:35:02 -0800

    library

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+gcc -c -Wall -Werror -Wextra -pedantic -fpic *.c
+gcc -shared -o liball.so *.o#!/bin/bash
+gcc -c -Wall -Werror -Wextra -pedantic -fpic *.c
+gcc -shared -o liball.so *.o#!/bin/bash
+gcc -c -Wall -Werror -Wextra -pedantic -fpic *.c
+gcc -shared -o liball.so *.o
