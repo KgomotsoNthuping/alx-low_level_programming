764bc0fc3b6c05a8856b43d9b3bc4d809c2bff0f
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-11-16 15:17:39 -0800

    lib

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * div - add
+ * @a: first
+ * @b: second
+ * Return: add
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * sub - sub
+ * @a: first
+ * @b: second
+ * Return: sub
+ */
+int subtract(int a, int b)
+{
+	return (a - b);
+}
