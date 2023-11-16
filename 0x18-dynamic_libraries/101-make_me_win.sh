a1e7b3513222ee0f21eb5b80c8f117e39f4c4479
Kgomotso222 <kgomotsonthuping22@gmail.com>
2023-11-16 15:48:00 -0800

    lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/gigalib.so "https://github.com/Kgomotso222/alx-low_level_programming/tree/master/0x18-dynamic_libraries/gigalib.so"
-export LD_PRELOAD=/tmp/gigalib.so
+wget -P ..  https://raw.githubusercontent.com/Kgomotso222/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD="$PWD/../libtest.so"
